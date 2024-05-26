62adfdbb05763d790037a16609d9a22ff94adc4b
fedotser <sereoja.fedot@stellarchain.io>
2024-05-26 15:05:17 +0300

    add authentication

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -51,8 +51,8 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('homepage');
-        }
+            return $this->redirectToRoute('app_login');
+            }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
