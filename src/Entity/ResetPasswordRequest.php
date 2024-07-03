244d53ec92959604d31b09d6415f8e78f799be20
Dev Fed <devfed@localhost.localdomain>
2024-07-03 20:36:52 +0300

    update annotations

--- src/Entity/ResetPasswordRequest.php
+++ src/Entity/ResetPasswordRequest.php
@@ -7,6 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestTrait;
 
+/**
+ * @IgnoreAnnotation("ORM\Column")
+ */
 #[ORM\Entity(repositoryClass: ResetPasswordRequestRepository::class)]
 class ResetPasswordRequest implements ResetPasswordRequestInterface
 {
