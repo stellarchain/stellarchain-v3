db6cce4ccb3c4775f1bbefc9f2d36a31d8226097
Dev Fed <devfed@localhost.localdomain>
2024-07-03 21:12:00 +0300

    disable emailing

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,13 +45,13 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('support@stellarchain.io', 'Stellar Chain Explorer'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+            /* $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user, */
+            /*     (new TemplatedEmail()) */
+            /*         ->from(new Address('support@stellarchain.io', 'Stellar Chain Explorer')) */
+            /*         ->to($user->getEmail()) */
+            /*         ->subject('Please Confirm your Email') */
+            /*         ->htmlTemplate('registration/confirmation_email.html.twig') */
+            /* ); */
 
             return $this->redirectToRoute('app_login');
         }
