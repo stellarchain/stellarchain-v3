ef765775d475477e18205ca831c3316bcdc79d36
Fedot Serghei <devfed@localhost.localdomain>
2024-08-30 19:59:14 +0300

    run build metrics at 1 minute

--- src/Scheduler/BuildAssetMetrics.php
+++ src/Scheduler/BuildAssetMetrics.php
@@ -6,7 +6,7 @@ use Symfony\Component\Scheduler\Attribute\AsCronTask;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
-#[AsCronTask('*/60 * * * *')]
+#[AsCronTask('1 * * * *')]
 class BuildAssetMetrics
 {
     public function __invoke(): void
