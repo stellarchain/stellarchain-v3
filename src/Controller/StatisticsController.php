a37a430b4134f4926e6bd714f9e868a90d7880ed
Fedot Serghei <devfed@localhost.localdomain>
2024-10-22 12:14:02 +0300

    add cache for long statistics

--- src/Controller/StatisticsController.php
+++ src/Controller/StatisticsController.php
@@ -7,13 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class StatisticsController extends AbstractController
 {
     #[Route('/statistics', name: 'app_statistics')]
     public function index(StatisticsService $statisticsService): Response
     {
-        $statisticsCharts = $statisticsService->buildStatisticsCharts();
+        $cache = new FilesystemAdapter();
+        $statisticsCharts = $cache->get('statistics_chart', function (ItemInterface $item) use ($statisticsService): array {
+            $item->expiresAfter(3600);
+            return $statisticsService->buildStatisticsCharts();
+        });
         return $this->render('statistics/index.html.twig', [
             'statistics_charts' => $statisticsCharts
         ]);
@@ -33,7 +39,7 @@ class StatisticsController extends AbstractController
     {
         $chartData = $statisticsService->getMetricsData($stat, $chart);
         $areaSeries = [];
-        foreach($chartData['label'] as $k => $time) {
+        foreach ($chartData['label'] as $k => $time) {
             $areaSeries[] = [
                 'value' => $chartData['data'][$k],
                 'time' => \DateTime::createFromFormat('m-d-Y H:i', $time)->getTimestamp()
